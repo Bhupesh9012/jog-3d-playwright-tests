a3412e423c379f8e89ffc686a17b68b8d69c91d4
Bhupesh9012 <joshibhupesh46@gmail.com>
2026-05-21 11:10:49 +0530

    21/5

--- Page-Objects/LoginPage.js
+++ Page-Objects/LoginPage.js
@@ -3,8 +3,18 @@ class LoginPage {
      constructor(page){
         this.page=page;
         this.signInLink=page.locator("//a[normalize-space()='sign-in']");
-        this.usernameInput=page.locator('#input[name="email"]');
-        this.pawwordInput=page.locator('#input[name="password"]');
+        this.usernameInput=page.locator("//input[@name='email']");
+        this.passwordInput=page.locator("//input[@name='password']");
         this.signInButton=page.locator("//button[normalize-space()='Sign in']");
      }
+     async navigate(){
+        await this.page.goto("https://3d.jog-joinourgame.com/3djogdigital/index.php");
+        await this.signInLink.click();
+     }
+        async login(username,password){
+            await this.usernameInput.fill(username);
+            await this.passwordInput.fill(password);
+            await this.signInButton.click();
+        }
 }
+module.exports={LoginPage};
--- tests/RosterDetails.spec.js
+++ tests/RosterDetails.spec.js
@@ -1,3 +1,9 @@
 const{test,expect}=require('@playwright/test');
+const{LoginPage}=require('../Page-Objects/LoginPage');
 test("Roster Details page",async function({page}){
+    const loginPage = new LoginPage(page);
+    await loginPage.navigate();
+    await loginPage.login('Tester123@gmail.com','Tester123');
+
+    await expect(page.locator("text=Welcome")).toBeVisible({ timeout: 10000 });
 })
